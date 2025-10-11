688ad03470c44b60acb5efd779af2f3c534d15f5
Gk.keshan <g.k.keshan0120@gmail.com>
2025-10-11 20:38:44 +0530

    Update Interface.c
    created loop for the repeating after entered invalid Officer ID

--- Interface.c
+++ Interface.c
@@ -2,71 +2,76 @@
 #include <stdlib.h>
 
 int main(){
-
-  //Variable declaration  
-  int choice,offid, authoid=1234;  
-
-  
-  printf("\t==== Election System ===\n\n");
-  
-  printf("Enter Officer ID: ");  //Enter Officer ID to start the system
-	scanf("%d", &offid);
-  
-  system("clear || cls"); // clear the screen after entered ID
-  
-
-  
- if(offid==authoid){     // officer id verification
-
-	do{ 
-		printf("\t==== Election System ===\n\n");
-		
-		//print options ask choice
-		
-		printf("1. Candidate Registration\n");
-		printf("2. Voter Registration\n");
-		printf("0. Exit \n\n");
-
-		printf("Enter Your Choice: ");
-		scanf("%d",&choice);
-		
-		system("clear || cls");  // clear the screen after select a option
-	
-	
-//call to switch to function the choices
-
-		switch(choice){
-
-			case 1:
-              printf("In a C R\n\n"); //entering the candidate registration
-			  
-			break;
-
-			case 2:
-               //entering the voter registration
-			break;
-
-			case 0:
 				printf("Exiting... \nDone\n");     //Exit in the program
-			break;   
-
-			default:
-			printf("Invalid Number! \n");    
+    //Variable declaration
+    int choice,offid, authoid=1234,g=0;
+    while (g<1) { //for repeating after entered invalid Officer ID
+        printf("\t==== Election System ===\n\n");
+        
+        printf("Enter Officer ID: ");  //Enter Officer ID to start the system
+        scanf("%d", &offid);
+        
+        system("clear || cls"); // clear the screen after entered ID
 
         
-		}
-	
-
-	}while(choice!=0); 
+        
+        if(offid==authoid){     // officer id verification
+            
+            do{
+                printf("\t==== Election System ===\n\n");
+                
+                //print options ask choice
+                
+                printf("1. Candidate Registration\n");
+                printf("2. Voter Registration\n");
+                printf("0. Exit \n\n");
+                
+                printf("Enter Your Choice: ");
+                scanf("%d",&choice);
+                
+                system("clear || cls");  // clear the screen after select a option
+                
+                
+                //call to switch to function the choices
+                
+                switch(choice){
+                        
+                    case 1:
+                        printf("In a C R\n\n"); //entering the candidate registration
+                        
+                        break;
+                        
+                    case 2:
+                        //entering the voter registration
+                        break;
+                        
+                    case 0:
+                        printf("Exiting... \nDone\n");
+                        g++;//need to end this after entered 0
+                        //Exit in the program
+                        break;
+                        
+                    default:
+                        printf("Invalid Number! \n");
+                        
+                        
+                }
+                
+                
+            }while(choice!=0);
+            
+        } 
+		else{
+            printf("Error! Invalid Officer ID !!!\n\n");
+			// If id is invalid, then go to start again
+            
+            
+        }
+    }
  
-} else{
-		printf("Error! Invalid Officer ID !!!\n\n"); 		// If id is invalid, then go to start again
-		//need to repeat this after enter invalid id
-
-		}
  
 
- return 0;    
+ return 0;
 }
 
 
